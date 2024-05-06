49109438bb93fc56cfd4423286cebbeee8bfe8ef
Maria <maria.cancian@aluno.senai.br>
2024-05-06 09:40:31 -0300

    feat: create tabela batalha

--- db/script.sql
+++ db/script.sql
@@ -12,3 +12,14 @@
          
         
 );
+
+CREATE TABLE batalhas(
+    id SERIAL PRIMARY KEY,
+    heroi1_id INT NOT NULL,
+    heroi2_id INT NOT NULL,
+    vencedor_id INT NOT NULL,
+    FOREIGN KEY (heroi1_id) REFERENCES herois(id),
+    FOREIGN KEY (heroi2_id) REFERENCES herois(id),
+    FOREIGN KEY (vencedor_id) REFERENCES herois(id)
+   
+);
